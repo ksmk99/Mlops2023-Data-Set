cd8875ee58d4bb6365efe07fe32409409db10f6c
Ksmk99 <kosarev.me@thegamix.com>
2023-03-19 14:25:56 +0500

    Pipeline commands update

--- Pipeline.sh
+++ Pipeline.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-Apt install python3-pip
-Pip install wget
-Pip install scikit-learn
-Pip install pandas
-Pip install requests
+apt install python3-pip
+pip install wget
+pip install scikit-learn
+pip install pandas
+pip install requests
 python3 data_creation.py
 python3 model_processing.py
 python3 model_preparation.py
